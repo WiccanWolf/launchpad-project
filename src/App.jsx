b91ea4559386da74d9ff76ff892802ba729c589c
Toni <dayhlprod@gmail.com>
2025-05-12 09:35:54 +0100

    Initialised Project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-blue-500 text-white p-8'>
+      <h1 className='text-4xl font-bold'>Hello, Tailwind CSS!</h1>
+      <p>Welcome to your Vite + React + Tailwind setup.</p>
+    </div>
+  );
+}
+
+export default App;
