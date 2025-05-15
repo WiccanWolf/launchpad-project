420926cc92c5cf9908214500cc43f774de0c24c2
Toni <dayhlprod@gmail.com>
2025-05-15 16:09:49 +0100

    Changed connection.js file to fix render issues

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,12 +1,25 @@
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const connectionString = process.env.ATLAS_URI || '';
-const client = new MongoClient(connectionString);
-let connect;
-try {
-  connect = await client.connect();
-  console.log('Connected to the Database.');
-} catch (error) {
-  console.error(error);
-}
-let db = connect.db('events_sample');
+const client = new MongoClient(connectionString, {
+  tls: true,
+  useUnifiedTopology: true,
+});
+
+let db;
+
+const connectToDb = async () => {
+  try {
+    await client.connect();
+    console.log(`Connected to the Database.`);
+    db = client.db('events_sample');
+  } catch (err) {
+    console.error(`Failed to connect to MongoDB: ${err}`);
+  }
+};
+
+await connectToDb();
+
 export default db;
