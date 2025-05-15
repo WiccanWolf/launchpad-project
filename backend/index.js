d2eceacd89cab8f7346b1f40461d2e0424c88ca6
Toni <dayhlprod@gmail.com>
2025-05-15 22:17:29 +0100

    Fixed MVC issues

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
-import './loadEnvironment.mjs';
+import dotenv from 'dotenv';
+dotenv.config();
 import { getEvents } from './routes/controllers/index.controller.js';
 
+const PORT = 5100;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send({ msg: 'Hello from the server!' });
-});
+mongoose.connect(process.env.ATLAS_URI, { dbName: 'events_sample' });
 
 app.get('/events', getEvents);
 
-export default app;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
